6eacdf55029f10013d3ca059bf039a77f2f3f867
Allisson Mateus Melo Reis <allisson.reis@tjto.jus.br>
2020-02-21 15:32:30 -0300

    Integracao com Unform

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import { Scope } from '@unform/core'
+import { Form } from '@unform/web'
+import ImageInput from './components/Form/ImageInput'
+import Input from './components/Form/Input'
+import Select from './components/Form/Select'
+import * as Yup from 'yup'
 
 import './styles/global.css'
 
-function App() {
+function App () {
+  const formRef = useRef(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      formRef.current.setData({
+        name: 'John Doe',
+        city: '1'
+      })
+    }, 2000)
+  }, [])
+
+  async function handleSubmit (data, { reset }) {
+    try {
+      // Remove all previous errors
+      formRef.current.setErrors({})
+      const schema = Yup.object().shape({
+        file: Yup.mixed().required('O arquivo é obrigatório'),
+        name: Yup.string().required('O nome é obrigatório')
+        //password: Yup.string().min(6).required(),
+      })
+      await schema.validate(data, {
+        abortEarly: false
+      })
+      // Validation passed
+      console.log(data)
+      reset()
+    } catch (err) {
+      const validationErrors = {}
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach(error => {
+          validationErrors[error.path] = error.message
+        })
+        formRef.current.setErrors(validationErrors)
+      }
+    }
+  }
+
   return (
-    <div id="app">
+    <div id='app'>
       <Header />
-      <main id="main">
-        <h1>Hello Word</h1>
-        <h1>Hello Word</h1>
-        <h1>Hello Word</h1>
+      <main id='main'>
+        <Form onSubmit={handleSubmit} ref={formRef}>
+          <ImageInput name='file' label='Arquivo' />
+          <Input name='name' label='Name' />
+          <Select
+            name='city'
+            label='Cidade'
+            options={[
+              { value: '1', label: 'Palmas' },
+              { value: '2', label: 'Açailandia' }
+            ]}
+          />
+          <button type='submit'>Enviar</button>
+        </Form>
       </main>
-      <Footer />  
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Form/ImageInput.js
@@ -0,0 +1,48 @@
+import React, {
+  ChangeEvent,
+  useRef,
+  useEffect,
+  useCallback,
+  useState
+} from 'react'
+import { useField } from '@unform/core'
+
+export default function ImageInput ({ name, label, ...rest }) {
+  const inputRef = useRef(null)
+  const { fieldName, registerField, defaultValue, error } = useField(name)
+  const [preview, setPreview] = useState(defaultValue)
+
+  const handlePreview = useCallback(e => {
+    const file = e.target.files?.[0]
+
+    if (!file) {
+      setPreview(null)
+    }
+    const previewURL = URL.createObjectURL(file)
+    setPreview(previewURL)
+  }, [])
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'files[0]',
+      clearValue (ref) {
+        ref.value = ''
+        setPreview(null)
+      },
+      setValue (_, value) {
+        setPreview(value)
+      }
+    })
+  }, [fieldName, registerField])
+
+  return (
+    <>
+      {label && <label htmlFor={fieldName}>{label}</label>}
+      {preview && <img src={preview} alt='Preview' width='100' />}
+      <input type='file' ref={inputRef} onChange={handlePreview} {...rest} />
+      {error && <span style={{ color: '#f00' }}>{error}</span>}
+    </>
+  )
+}
